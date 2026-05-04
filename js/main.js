65a2065548c42caffd024950b7007a5cef5a3855
KHALED ELFAHL <khaledelfahl56@gmail.com>
2026-05-04 22:27:05 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let scroll = document.querySelector('.scroll');
+
+window.onscroll = function () {
+    if (window.scrollY > 200) {
+        scroll.classList.add('show');
+    } else {
+        scroll.classList.remove('show');
+    }
+}
+
+scroll.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth'
+    })
+}
